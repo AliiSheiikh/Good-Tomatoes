4df145d26407a4c736efd4a9e480e3c40693bd6d
Ali Sheikh <l_lawliet786@hotmail.com>
2025-08-13 22:04:27 -0400

    added components and css styling for movies and watched movies

---
+++ src/Components/MovieWatchedBox.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { tempMovieData, tempWatchedData } from "../App";
+import WatchedMovie from "./WatchedMovie";
+
+export default function MovieWatchedBox({ movies }) {
+  const [isOpen2, setIsOpen2] = useState(true);
+
+  const average = (arr) =>
+    arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
+  const [watched, setWatched] = useState(tempWatchedData);
+
+  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+  const avgUserRating = average(watched.map((movie) => movie.userRating));
+  const avgRuntime = average(watched.map((movie) => movie.runtime));
+
+  return (
+    <div className="box">
+      <button
+        className="btn-toggle"
+        onClick={() => setIsOpen2((open) => !open)}
+      >
+        {isOpen2 ? "–" : "+"}
+      </button>
+      {isOpen2 && (
+        <>
+          <div className="summary">
+            <h2>Movies you watched</h2>
+            <div>
+              <p>
+                <span>#️⃣</span>
+                <span>{watched.length} movies</span>
+              </p>
+              <p>
+                <span>⭐️</span>
+                <span>{avgImdbRating}</span>
+              </p>
+              <p>
+                <span>🌟</span>
+                <span>{avgUserRating}</span>
+              </p>
+              <p>
+                <span>⏳</span>
+                <span>{avgRuntime} min</span>
+              </p>
+            </div>
+          </div>
+
+          <ul className="list">
+            {watched.map((movie) => (
+              <WatchedMovie movie={movie} />
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
+}
